11d5596e086f9f492039ecdf7265db76b278dea0
andersonfebs <andersonfebs@gmail.com>
2020-06-22 23:59:03 -0400

    maximize function

--- desafios-módulo-2/scripts.js
+++ desafios-módulo-2/scripts.js
@@ -6,12 +6,23 @@ for (let card of cards) {
         modalOverlay.classList.add('active');
         const pageId = card.getAttribute("id");
 
-        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${pageId}`;       
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${pageId}`;
     });
 }
+
+document.querySelector('.maximize-modal').addEventListener('click', function() {
+    const modalMaximize = modalOverlay.querySelector('.modal').classList.contains('maximize');
+
+    if (modalMaximize) {
+        modalOverlay.querySelector('.modal').classList.remove("maximize");
+    } else {
+        modalOverlay.querySelector('.modal').classList.add("maximize");
+    }    
+});
 
 document.querySelector('.close-modal').addEventListener('click', function() {
     modalOverlay.classList.remove("active");
     modalOverlay.querySelector("iframe").src = "";
 });
+
 
