19e7496d35675ac9b778dee11169ab538665025c
andersonfebs <andersonfebs@gmail.com>
2020-06-24 09:45:08 -0400

    challenge 3.2

---
+++ desafios-módulo-3/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id: "starter",
+        image: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        description: "Para quem não programou e ou é programador iniciante e quer dar uma acelerada nos estudos.",
+        modules: "5 modules",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        image: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        description: "Para programadores iniciantes e querem entrar na área do jeito certo.",
+        modules: "7 módulos",
+        price: "Pago"
+    },
+    {
+        id: "gostack",
+        image: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        description: "Para quem já trabalha na área a algum tempo e quer dar um up na sua carreira.",
+        modules: "10 modules",
+        price: "Pago"
+    }
+]
--- desafios-módulo-3/server.js
+++ desafios-módulo-3/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const courses = require('./data');
 
 const server = express();
 
@@ -8,15 +9,40 @@ server.set("view engine", "njk");
 server.use(express.static('public'));
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 });
 
 server.get("/", function(req, res) {
-    return res.render("about");
+
+    const about = {
+        image: "https://rocketseat.com.br/static/images/logo-rocketseat.svg",
+        title1: "Rocketseat",
+        title2: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        description: 'No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa. Mais informações confira em <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>.',
+        technologies: ["React", "NodeJS", "ReactNative"],
+        links: [
+            {
+                url: "https://github.com/rocketseat",
+                name: "Github"
+            },
+            {
+                url: "https://www.instagram.com/rocketseat_oficial/",
+                name: "Instagram"
+            },
+            {
+                url: "https://www.facebook.com/rocketseat",
+                name: "Facebook"
+            }
+        ]
+    }
+
+
+    return res.render("about", { about });
 });
 
 server.get("/courses", function(req, res) {
-    return res.render("courses");
+    return res.render("courses", { items: courses });
 })
 
 server.listen(5000, function() {
